772374146a37c455a5284f5387935605b175a4c7
leonid <forestmarket@yandex.ru>
2022-05-20 22:06:47 +0300

    v1

---
+++ src/components/download.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Download = () => {
+    return (
+
+            <div id={'download'}>
+                <div id={'content'}>
+                    <p className={'text-white'}>Идет загрузка....</p>
+                    <p id={'rotate'} className={'text-white'}>&#8635;</p>
+
+                </div>
+
+            </div>
+    );
+};
+
+export default Download;
